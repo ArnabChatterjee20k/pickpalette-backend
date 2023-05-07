d6fa600e0916e41051104676ded671abf9b410b5
arnab <arnabchatterjee.ac.2@gmail.com>
2023-05-07 22:08:14 +0530

    added cors

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ import express from "express";
 import router from "./v1/route/newsletter.js";
 import getIP from "./utils/getIP.js";
 import * as dotenv from 'dotenv' 
+import cors from 'cors';
 dotenv.config(".env")
 
 
 const app = express()
 const PORT = process.env.PORT
 
+app.use(cors())
+
 app.use("/newsletter",router)
+
 
 app.listen(PORT, getIP(), () => {
     console.log(`Server listening on http://${getIP()}:${PORT}`);
